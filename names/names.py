82363606c9fc23d89d8e0c82f7829055163b8a55
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-28 18:28:35 -0600

    names.py converted to BST

--- names/names.py
+++ names/names.py
@@ -13,10 +13,50 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+class BSTNode:
+    def __init__ (self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+    def insert(self, value):
+        if value >= self.value:
+            if self.right == None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        else:
+            if self.left == None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            duplicates.append(target)
+        if target > self.value:
+            if self.right is None:
+                return False
+            else:
+                self.right.contains(target)
+        else:
+            if self.left is None:
+                return False
+            else:
+                self.left.contains(target)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+names_BST = BSTNode(names_1[0])
+for each in names_1:
+    names_BST.insert(each)
+
+for each in names_2:
+    names_BST.contains(each)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
